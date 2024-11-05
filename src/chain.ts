eb38358f3d0508b3506c6043be16d3bb00c78133
w1nt3r-eth <w1nt3r.eth@gmail.com>
2024-11-04 18:56:37 -0800

    Use production metadata contract

--- src/chain.ts
+++ src/chain.ts
@@ -10,7 +10,7 @@ import { base } from "viem/chains";
 
 export const BASEPAINT_ADDRESS = "0xBa5e05cb26b78eDa3A2f8e3b3814726305dcAc83";
 export const BRUSH_ADDRESS = "0xD68fe5b53e7E1AbeB5A4d0A6660667791f39263a";
-export const METADATA_ADDRESS = "0xcfb86b6aC2cE09f9A01C39af9Dccf3ecba304F95";
+export const METADATA_ADDRESS = "0x5104482a2Ef3a03b6270D3e931eac890b86FaD01";
 
 export const client = createWalletClient({
   transport: custom((window as any).ethereum),
