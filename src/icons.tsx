44b45c3b1b61bea3a352029c126f05dec8efa26b
cristhian urrego <infoafuro@gmail.com>
2025-01-17 22:59:50 -0500

    Eraser button for touch devices (#2)
    * easer icon
    
    * easer method

--- src/icons.tsx
+++ src/icons.tsx
@@ -105,3 +105,26 @@ export function HandIcon() {
     </svg>
   );
 }
+
+export function EraserIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 7.5l16.5 9"
+      />
+    </svg>
+  );
+}
